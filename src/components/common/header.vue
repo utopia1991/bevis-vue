47d935dc075810d51961380edae84c224277d3db
Bevis Shen <1304354608@qq.com>
2017-03-08 10:26:46 +0800

    add about

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -45,9 +45,9 @@
 		</div>
 		<div class="nav">
 			<router-link to="/pirates" class="nav-title">路飞海贼团</router-link>
-			<ul class="sub_nav">
+			<ul>
 				<!-- 可以传给 v-bind:class 一个对象，以动态地切换 class。注意 v-bind:class 指令可以与普通的 class 特性共存 -->
-				<li v-for="tab in tabs" v-bind:class="{ current:tab.isActive }">
+				<li v-for="tab in tabs" :class="{ current:tab.isActive }">
 					<!-- 使用 <router-link> 创建 a 标签来定义导航链接 -->
 					<router-link :to="tab.url">{{tab.name}}</router-link>
 				</li>
